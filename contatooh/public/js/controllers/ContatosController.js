9e0efcdea35fe6bc108b463fce353ffaa3706a96
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-04-28 18:40:53 -0300

    criando lista fixa de contatos

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -3,4 +3,21 @@ angular.module('contatooh').controller('ContatosController', function ($scope){
   $scope.incrementa = function(){
     $scope.total++;
   };
+  $scope.contatos = [
+    {
+      "_id": 1,
+      "nome": "João da Silva 1",
+      "email": "joao1@teste.com"
+    },
+    {
+      "_id": 2,
+      "nome": "João da Silva 2",
+      "email": "joao2@teste.com"
+    },
+    {
+      "_id": 3,
+      "nome": "João da Silva 3",
+      "email": "joao3@teste.com"
+    }
+  ];
 });
