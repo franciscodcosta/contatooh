cbc74c67785ff34b7bf705a381b5606531c00433
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-05-03 21:13:08 -0300

    correção - parênteses a mais

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -11,7 +11,7 @@ angular.module('contatooh').controller('ContatosController', function ($scope, $
   .success(function(data){
     $scope.contatos = data;
   })
-  .error(function(statusText)){
+  .error(function(statusText){
     console.log("Não foi possível obter a lista de contatos");
     console.log(statusText);
   });
