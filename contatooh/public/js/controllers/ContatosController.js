97263809fd9cba857873707f6f5aea82323cecc7
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-05-05 08:10:54 -0300

    REST Endpoints com $resource

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -2,13 +2,18 @@ angular.module('contatooh').controller('ContatosController', function ($scope, $
   $scope.contatos = [];
   $scope.filtro ='';
   
-  $http.get('/contatos')
-  .success(function(data){
-    $scope.contatos = data;
-  })
-  .error(function(statusText){
-    console.log("Não foi possível obter a lista de contatos");
-    console.log(statusText);
-  });
+  var Contato = $resource('/contatos/:id');
+  function buscaContatos(){
+    Contato.query(
+      function(contatos){
+        $scope.contatos = contatos;
+      },
+      function (erro){
+        console.log("Não foi possível obter a lista de contatos.");
+        console.log(erro);
+      }
+      );
+  }
+  buscaContatos();
   
 });
