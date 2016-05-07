1a4b90d8c5400049205a4b3f983eb205eb99c703
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-05-07 09:32:31 -0300

    novo contato

--- contatooh/public/js/controllers/ContatoController.js
+++ contatooh/public/js/controllers/ContatoController.js
@@ -15,7 +15,18 @@ angular.module('contatooh').controller('ContatoController',
         });
       
     } else {
-      $scope.contato = {};
+      $scope.contato = new Contato();
     }
+    
+    $scope.salva = function(){
+      $scope.contato.$save()
+        .then(function(){
+          $scope.mensagem = {texto: 'Salvo com sucesso'};
+          $scope.contato = new Contato();
+        })
+        .catch(function (erro){
+          $scope.mensagem = {texto: 'Não foi possível salvar'};
+        });
+    };
     
 });
