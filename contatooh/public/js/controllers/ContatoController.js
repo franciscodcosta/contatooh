de74fabe13375d720d6a109f69006e2b91917350
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-05-07 10:33:54 -0300

    service Contato

--- contatooh/public/js/controllers/ContatoController.js
+++ contatooh/public/js/controllers/ContatoController.js
@@ -1,7 +1,6 @@
 angular.module('contatooh').controller('ContatoController', 
-  function($scope, $routeParams, $resource) {
-    var Contato =$resource('/contatos/:id');
-  
+  function($scope, $routeParams, Contato) {
+    
     if ($routeParams.contatoId){
       Contato.get({id: $routeParams.contatoId},
         function(contato){
