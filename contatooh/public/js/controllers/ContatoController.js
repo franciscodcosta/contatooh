d9feef89aee666ac142d1c55ae547fc38ebf91c7
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-05-07 09:19:46 -0300

    novo contato

--- contatooh/public/js/controllers/ContatoController.js
+++ contatooh/public/js/controllers/ContatoController.js
@@ -2,15 +2,20 @@ angular.module('contatooh').controller('ContatoController',
   function($scope, $routeParams, $resource) {
     var Contato =$resource('/contatos/:id');
   
-    console.log($routeParams.contatoId);
-    Contato.get({id: $routeParams.contatoId},
-      function(contato){
-        $scope.contato = contato;
-      },
-      function (erro){
-        $scope.mensagem = {
-          texto: 'Não foi possível obter o contato.'
-        };
-        console.log(erro);
-      });
+    if ($routeParams.contatoId){
+      Contato.get({id: $routeParams.contatoId},
+        function(contato){
+          $scope.contato = contato;
+        },
+        function (erro){
+          $scope.mensagem = {
+            texto: 'Não foi possível obter o contato.'
+          };
+          console.log(erro);
+        });
+      
+    } else {
+      $scope.contato = {};
+    }
+    
 });
