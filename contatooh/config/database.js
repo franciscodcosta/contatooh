9bf1079cac182aa8758b2e5338e2109455699fea
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-05-15 09:06:11 -0300

    conexão ao MongoDB

--- contatooh/config/database.js
+++ contatooh/config/database.js
@@ -1,4 +1,13 @@
 var mongoose = require('mongoose');
 module.exports = function(uri){
 	mongoose.connect(uri);
+	mongoose.connection.on('connected', function(){
+		console.log('Mongoose! Conectado em ' + uri);
+	});
+	mongoose.connection.on('disconnected', function(){
+		console.log('Mongoose! Desconectado de ' + uri);
+	});
+	mongoose.connection.on('error', function(erro){
+		console.log('Mongoose! Erro na conexao ' + erro);
+	});
 }
