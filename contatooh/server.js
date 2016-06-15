3b61066fb394a4531145dd1e29407bfa22daed9a
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-06-15 19:30:06 -0300

    testando conexão ao mongo

--- contatooh/server.js
+++ contatooh/server.js
@@ -1,6 +1,6 @@
 var http = require ('http');
 var app = require('./config/express')();
-require('./config/database.js')('mongodb://104.197.173.17/contatooh');
+require('./config/database.js')('mongodb://104.197.173.17/test');
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express Server escutando na porta ' + app.get('port'));
 });
