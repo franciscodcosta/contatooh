513a3b2be088e2a8708a0fd47f1b026f239e1fbb
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-06-24 19:07:11 -0300

    ip mongo

--- contatooh/server.js
+++ contatooh/server.js
@@ -1,6 +1,6 @@
 var http = require ('http');
 var app = require('./config/express')();
-require('./config/database.js')('mongodb://10.128.0.2/test');
+require('./config/database.js')('mongodb://104.197.149.0/test');
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express Server escutando na porta ' + app.get('port'));
 });
