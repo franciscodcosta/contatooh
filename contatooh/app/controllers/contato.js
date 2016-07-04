761e3ffff21cdcab99dc33b456493964013d84f5
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-07-04 18:47:21 -0300

    listaContatos - MongoDB

--- contatooh/app/controllers/contato.js
+++ contatooh/app/controllers/contato.js
@@ -1,7 +1,18 @@
 module.exports = function(app){
 	var Contato = app.models.contato;
 	var controller = {};
-	controller.listaContatos = function(req, res){};
+	controller.listaContatos = function(req, res){
+		Contato.find().exec()
+		.then(
+			function(contatos){
+				res.json(contatos);
+			},
+			function(erro){
+				console.error(erro);
+				res.status(500).json(erro);
+			}
+		);
+	};
 	controller.obtemContato = function(req, res){};
 	controller.removeContato = function (req, res){};
 	controller.salvaContato = function (req, res){};
