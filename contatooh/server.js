858e8bee9584d965a3381ba629a562e6f0fc4b64
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-07-04 18:31:26 -0300

    conexão do BD Contatooh

--- contatooh/server.js
+++ contatooh/server.js
@@ -1,6 +1,6 @@
 var http = require ('http');
 var app = require('./config/express')();
-require('./config/database.js')('mongodb://10.128.0.5/test');
+require('./config/database.js')('mongodb://10.128.0.5/contatooh');
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express Server escutando na porta ' + app.get('port'));
 });
