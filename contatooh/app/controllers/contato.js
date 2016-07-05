6d46d1d1d2070d43661ba73af11e26f528637f9c
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-07-05 19:00:06 -0300

    populate para retornar emergência

--- contatooh/app/controllers/contato.js
+++ contatooh/app/controllers/contato.js
@@ -2,7 +2,7 @@ module.exports = function(app){
 	var Contato = app.models.contato;
 	var controller = {};
 	controller.listaContatos = function(req, res){
-		Contato.find().exec()
+		Contato.find().populate('emergencia').exec()
 		.then(
 			function(contatos){
 				res.json(contatos);
