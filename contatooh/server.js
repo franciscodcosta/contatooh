aef02ca83d2ceac2df39ebd7c192f7187e0144f0
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-09-15 16:43:11 -0300

    corrigindo commit errado

--- contatooh/server.js
+++ contatooh/server.js
@@ -1,5 +1,6 @@
 var http = require ('http');
-http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('servidor node de teste.\n');
-}).listen(3000, '127.0.0.1');
+var app = require('./config/express')();
+require('./config/database.js')('mongodb://10.128.0.5/contatooh');
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express Server escutando na porta ' + app.get('port'));
+});
