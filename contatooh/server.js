864518bf9e1ba640a17c12124fc6aec6cd65ead9
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-09-15 16:38:54 -0300

    configurando node

--- contatooh/server.js
+++ contatooh/server.js
@@ -1,6 +1,5 @@
 var http = require ('http');
-var app = require('./config/express')();
-require('./config/database.js')('mongodb://10.128.0.5/contatooh');
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server escutando na porta ' + app.get('port'));
-});
+http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('servidor node de teste.\n');
+}).listen(3000, '127.0.0.1');
